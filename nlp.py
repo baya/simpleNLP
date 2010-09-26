e827ff9d56cf9b7a2ffec1970ac98efd85965fed
rex <rex@zhasm.com>
2010-09-26 22:17:35 +0800

    main program updated

--- nlp.py
+++ nlp.py
@@ -7,8 +7,8 @@
 #created:        2010-09-26 19:15
 
 import re
+import sys
 
-regex=re.compile(r"(?x) (?: [\w-]+  | [\x80-\xff]{3} )")
 
 def init_wordslist(fn="./words.txt"):
     f=open(fn)
@@ -46,13 +46,21 @@ def search_in_trie(chars, trie):
             except:
                 pass
             break
+def main():
+    #init
+    regex=re.compile(r"(?x) (?: [\w-]+  | [\x80-\xff]{3} )")
+    words=init_wordslist()
+    trie=words_2_trie(words)
+    #read content
+    fn=sys.argv[1]
+    string=open(fn).read()
+    chars=regex.findall(string)
+    
+    #do the job
+    search_in_trie(chars, trie)
 
-words=init_wordslist()
-trie=words_2_trie(words)
-string="很美观对不对奇妙的是对于第一条字串它生成的结构是这样的；对于新插入的第二条，第三条……第N条字串，它不是另起炉炉灶，而是萧规曹随，见缝插针，充分利用前面已经成生的数据结构。这要归功于Hash/Dictionary这种数据结构的特点。看一下针对于foobar foobah fooxar foozap fooza 完全插入后的效果 理性爱国 性爱好神奇 我爱正则表达式"
-chars=regex.findall(string)
-search_in_trie(chars, trie)
-
+if __name__=='__main__':
+    main()
 
 
 
